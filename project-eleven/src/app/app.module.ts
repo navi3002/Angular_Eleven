69045e8c7e3fa8d7c5bf6f30b9fff516025b4b82
navi3002 <navithapzal.mohamedrafee@chainsys.com>
2022-05-04 09:40:36 +0530

    first commit

--- project-eleven/src/app/app.module.ts
+++ project-eleven/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EmpformComponent } from './empform/empform.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpformComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
